ed941ee372b111d984f55d6647c3d2ba8faf7554
Arjun <arjunyadavdesigns@gmail.com>
2025-02-01 18:09:43 +0530

    modify template
    - changed title on index
    - added comments for context on sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-//unnamed; month, year. 
+//inkBlot_1d; february, 2025. 
+
+//shobhan explained his logic for the ink blot and asked me to create a one-dimensional simulation. 
 
 function setup() {
 createCanvas(1000, 562); //in 16:9 aspect ratio. 
 }
 
 function draw() {
-background(0);
+background(255);
 }
