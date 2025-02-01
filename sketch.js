de6138f96c9c941cf92e0a878935a2f67c08955c
Arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-02-01 18:29:47 +0530

    initial setup (#1)
    * add starting template
    
    * modify template
    
    - changed title on index
    - added comments for context on sketch.js
    
    * add style.css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+//inkBlot_1d; february, 2025. 
+
+//shobhan explained his logic for the ink blot and asked me to create a one-dimensional simulation. 
+
+function setup() {
+createCanvas(1000, 562); //in 16:9 aspect ratio. 
+}
+
+function draw() {
+background(255);
+}
