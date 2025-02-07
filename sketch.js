a06da04bb02984b9e3d2a8ecc52c7c14a9f1619e
Arjun <arjunyadavdesigns@gmail.com>
2025-02-07 11:36:14 +0530

    update past code

--- sketch.js
+++ sketch.js
@@ -1,62 +1,76 @@
-//inkBlot_1d; february, 2025. 
-
-//shobhan explained his logic for the ink blot and asked me to create a one-dimensional simulation. 
+//inkblot_1d_v2; february 2025. 
 
 /*
 approach: 
 
-- there is going to be a 'surface' that is comprised of many 'cells'. 
-- since it's a one-dimensional surface, the cells will be arranged from left to right.
+- there is a 'surface' that has many 'cells'. in code, the screen is the surface and the cells are defined in 'class Cell'.
 
-- each cell has: 
-    - position (x,y)
-    - size (w,h)
-    - capacity (how much of something can it store)
-    - the ability to tell each ink particle inside it to move. 
+- each cell has:
+    - position {x,y} on the surface
+    - size {w,h} (we assume a 'cell' is rectangular)
+    - capacity (how much of something can it hold)
+    - ability to 'speak' 
 
-- then, there's also an 'ink particle'. an ink particle has:
-    - position (x,y)
-    - size (r)
-    - move function (which is called by the cell). parameters there: 
-        - destination position (destX, destY)
-        - speed 
-        - condition to stop moving / marker to tell the cell that it has moved. 
+- an ink molecule has:
+    - position {x,y}
+    - size {r}
 
-cells and ink particles talk to each other. the cell tells the particle to move, the particle tells the cell that it has moved. 
+- 
 */
 
+//global declarations: 
 let cells = []; 
 const cellWidth = 10; //this is also the same width for the ink particles. 
+let cellHeight = 0; 
 const capacity = 2; //this is the capacity of what each cell can store. 
 
-let inkParticles = []; 
+let inkMolecules = []; 
 
 function setup() {
 createCanvas(1000, 562); //in 16:9 aspect ratio.
-background(255); //the background only needs to be drawn once.  
+cellHeight = height; //make cellHeight the same as canvas height. 
+rectMode (CENTER);  
 
-//create cell & ink
-for (let x = 0; x<=width; x+=cellWidth){
-cells.push(new Cell (x, 0, cellWidth, height)); 
+background(255); //the background only needs to be drawn once. 
+
+createSurface();
+createInk(); 
 }
-for (let y = 0+cellWidth/2; y<=height-cellWidth/2; y+=cellWidth){
-inkParticles.push(new InkParticle(cells[0].x+cellWidth/2, y, cellWidth)); 
+
+function createSurface(){
+//create cells
+for (let x = 0+cellWidth/2; x<=width-cellWidth/2; x+=cellWidth){
+cells.push(new Cell (x, 0+cellHeight/2, cellWidth, cellHeight)); 
+}
+}
+
+function createInk(){
+// create ink molecules
+
+for (let y = cells[0].y-(cellHeight/2)+cellWidth/2; y<=cellHeight-cellWidth/2; y+=cellWidth){
+inkMolecules.push(new InkMolecule(cells[0].x, y)); 
 }
 
 }
 
 function draw() {
+cellFunctions(); 
+inkFunctions(); 
 
+noLoop();
+}
+
+function cellFunctions(){
 for (cell of cells){
 cell.display(); 
-setTimeout(cell.checkContents(), 50000); 
+cell.checkContents(); 
+}
 }
 
-for (inkParticle of inkParticles){
-inkParticle.display(); 
+function inkFunctions(){
+for (molecule of inkMolecules){
+molecule.display(); 
 }
-
-//noLoop(); 
 }
 
 //cell object. 
@@ -79,11 +93,50 @@ rect (this.x, this.y, this.w, this.h);
 }
 
 checkContents(){
+//this checks how many ink molecules are inside each cell and stores them in an array. i use simple bounding box detection for this. 
+
+this.inkInside = []; 
+
+for (let i = 0; i<inkMolecules.length; i++){
+let molecule = inkMolecules [i]; 
+
+if (
+molecule.x >= this.x - this.w / 2 &&
+molecule.x < this.x + this.w / 2 &&
+molecule.y >= this.y - this.h / 2 &&
+molecule.y < this.y + this.h / 2
+) {
+this.inkInside.push(i);
+}
+}
+}
+
+}
+
+//ink particle object. 
+class InkMolecule{
+constructor(x, y, d = cellWidth){
+this.x = x; 
+this.y = y; 
+this.d = d; 
+}
+
+display(){
+noStroke(); 
+
+fill (0); 
+square (this.x, this.y, this.d); 
+}
+
+}
+
+/*
+checkContents(){
 //this checks how many ink particles are inside each cell and stores which of them are inside, in an array.
 this.inkInside = []; 
 
-for (let i = 0; i<inkParticles.length; i++){
-if (inkParticles[i].x<this.x+this.w && inkParticles[i].x>this.x){
+for (let i = 0; i<inkMolecules.length; i++){
+if (inkMolecules[i].x<this.x+this.w && inkMolecules[i].x>this.x){
 this.inkInside.push(i); 
 }
 }
@@ -95,28 +148,8 @@ console.log(this.inkInside);
 
 for (let i = 0; i<this.inkInside.length; i++){
 let particleIndex = this.inkInside[i]; 
-inkParticles[particleIndex].move(); 
+inkMolecules[particleIndex].move(); 
 }
 }
 }
-
-}
-
-//ink particle object. 
-class InkParticle{
-constructor(x, y, r){
-this.x = x; 
-this.y = y; 
-this.r = r; 
-}
-
-display(){
-strokeWeight (this.r); 
-stroke (0); 
-point (this.x, this.y); 
-}
-
-move(){
-this.x = lerp(this.x, this.x+cellWidth*2, 0.005);
-}
-}
+*/
