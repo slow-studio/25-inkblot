67c7e665226a121fe48c1e98267c8b17dc997689
Arjun <arjunyadavdesigns@gmail.com>
2025-02-14 16:50:22 +0530

    tried

--- sketch.js
+++ sketch.js
@@ -1,204 +1,125 @@
-//inkblot_1d_mathematical; february 2025. 
+// inkblot_2d_mathematical; February 2025.
+// Converted from 1D to 2D with faster offloading.
 
-//shobhan and vivek suggested to make a matrix and manipulate that, instead of drawing shapes; since p5's alpha was producing unintended effects. 
+let cells = [];
+const cellSize = 2; // Square cells
+const capacity = 255;
+let numRows, numCols;
 
-/*
-approach: 
-
-- there is a 'surface', made up of many 'cells', on which 'ink molecules' are dropped. in this program, the screen is the surface, cells are defined in class Cell and ink molecules are defined in class InkMolecule. 
-
-- each cell has
-    - position: {x, y} on surface
-    - size: {w, h}
-    - how much ink can it hold: {capacity}
-    - grayscale-value: {c} 
-    - ability to find adjacent neighbours
-    - ability to change colours based on number of ink molecules. 
-    - ability to offload ink molecules if they are greater than capacity. 
-
-an ink molecule has: 
-    - position :{x,y}
-    - ability to move to a new position, at a rate or speed. 
-*/
-
-//global declarations: 
-let cells = []; 
-const cellWidth = 5; //this is also the same width for the ink particles. 
-let cellHeight = 562; 
-const capacity = 255; //this is the capacity of what each cell can store. 
-
-let inkMolecules = []; 
-let numOfInkMolecules = 10000; 
+let inkMolecules = [];
+let numOfInkMolecules = 1000;
 
 function setup() {
-createCanvas(1000, 562); //in 16:9 aspect ratio.
-cellHeight = height; //make cellHeight the same as canvas height. 
-rectMode (CENTER);  
-
-background(255); //the background only needs to be drawn once. 
-
-createSurface();
-createInk(); 
+    createCanvas(500, 500, WEBGL); // Square canvas for simplicity
+    numCols = width / cellSize;
+    numRows = height / cellSize;
+    rectMode(CENTER);
+    background(255);
+    createSurface();
+    createInk();
 }
 
-function createSurface(){
-//create cells
-let iteration = 0; 
-for (let x = 0+cellWidth/2; x<=width-cellWidth/2; x+=cellWidth){
-cells.push(new Cell (x, 0+cellHeight/2, cellWidth, cellHeight, iteration)); 
-iteration++; 
+function createSurface() {
+    for (let y = 0; y < numRows; y++) {
+        for (let x = 0; x < numCols; x++) {
+            cells.push(new Cell(x, y));
+        }
+    }
+    for (let cell of cells) {
+        cell.findNeighbours();
+    }
 }
 
-//make each cell find their neighbours.
-for (let cell of cells){
-cell.findNeighbours(); 
-}
-}
-
-function createInk(){
-// create ink molecules
-
-//constructor(x, y, d = cellWidth)
-
-for (let i = 0; i<numOfInkMolecules; i++){
-inkMolecules.push (new InkMolecule(cells[cells.length/2].x, cells[cells.length/2].y)); 
-}
+function createInk() {
+    for (let i = 0; i < numOfInkMolecules; i++) {
+        inkMolecules.push(new InkMolecule(width/2, height/2));
+    }
 
 }
 
 function draw() {
-cellFunctions(); 
-//inkFunctions(); 
 
-filter (INVERT);
+    translate (-width/2, -height/2); 
+    cellFunctions();
+    filter(INVERT);
 }
 
-function cellFunctions(){
-for (let cell of cells){
-cell.display(); 
-cell.checkContents(); 
-cell.offloadInk(); 
-}
-}
-
-function inkFunctions(){
-for (let molecule of inkMolecules){
-molecule.display(); 
-}
+function cellFunctions() {
+    for (let cell of cells) {
+        cell.display();
+        cell.checkContents();
+        cell.offloadInk();
+    }
 }
 
-//cell object. 
-class Cell{
-constructor(x, y, w, h, index){
-this.x = x; 
-this.y = y; 
-this.w = w; 
-this.h = h; 
+class Cell {
+    constructor(gridX, gridY) {
+        this.gridX = gridX;
+        this.gridY = gridY;
+        this.x = gridX * cellSize + cellSize / 2;
+        this.y = gridY * cellSize + cellSize / 2;
+        this.capacity = capacity;
+        this.inkInside = [];
+        this.excessInk = [];
+        this.neighbours = [];
+        this.c = 255;
+    }
 
-this.capacity = capacity; 
-this.inkInside = []; 
-this.excessInk = []; 
+    findNeighbours() {
+        for (let dy = -1; dy <= 1; dy++) {
+            for (let dx = -1; dx <= 1; dx++) {
+                if (dx === 0 && dy === 0) continue;
+                let nx = this.gridX + dx;
+                let ny = this.gridY + dy;
+                if (nx >= 0 && nx < numCols && ny >= 0 && ny < numRows) {
+                    this.neighbours.push(cells[ny * numCols + nx]);
+                }
+            }
+        }
+    }
 
-this.index = index; 
-this.leftNeighbour = null;
-this.rightNeighbour = null;
+    display() {
+        fill(this.inkInside.length);
+        noStroke();
+        rect(this.x, this.y, cellSize, cellSize);
+    }
 
-this.c = 255; 
+    checkContents() {
+        this.inkInside = inkMolecules.filter(molecule =>
+            molecule.x >= this.x - cellSize / 2 &&
+            molecule.x < this.x + cellSize / 2 &&
+            molecule.y >= this.y - cellSize / 2 &&
+            molecule.y < this.y + cellSize / 2
+        );
+
+        this.excessInk = this.inkInside.length > this.capacity ?
+            this.inkInside.slice(this.capacity) : [];
+    }
+
+    offloadInk() {
+        if (this.excessInk.length > 0) {
+            for (let ink of this.excessInk) {
+                let target = random(this.neighbours);
+                ink.move(target.x, target.y, 0.5); // Increased movement speed
+            }
+        }
+    }
 }
 
-//cells find their neighbours on the surface. 
-findNeighbours(){
-if (this.index>0){
-this.leftNeighbour = cells[this.index - 1]; //left cell
-}
-if (this.index < cells.length - 1) {
-this.rightNeighbour = cells[this.index + 1]; // right cell
-}
+class InkMolecule {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
-}
-
-display(){
-stroke (0);
-strokeWeight (0); 
-
-console.log(this.c); 
-
-//this.c = map(this.inkInside.length, 0, inkMolecules.length, 255, 0);
-// this.c = min(this.inkInside.length, this.capacity); // Ensure it doesn't exceed 255
-// fill(255 - this.c); // Inverts grayscale logic
-
-this.c = this.inkInside.length; 
-fill (this.c); 
-
-
-rect (this.x, this.y, this.w, this.h);
+    move(destX, destY, speed = 0.1) {
+        this.x = lerp(this.x, destX, speed);
+        this.y = lerp(this.y, destY, speed);
+    }
 }
 
-checkContents(){
-//this checks how many ink molecules are inside each cell and stores them in an array. i use simple bounding box detection for this. 
-
-this.inkInside = []; 
-
-for (let molecule of inkMolecules){
-if (
-molecule.x >= this.x - this.w / 2 &&
-molecule.x < this.x + this.w / 2 &&
-molecule.y >= this.y - this.h / 2 &&
-molecule.y < this.y + this.h / 2
-) {
-this.inkInside.push(molecule);
-}
-}
-
-//check whether the cell has too much ink. 
-if (this.inkInside.length> this.capacity){
-this.excessInk = this.inkInside.slice(this.capacity); //make a new array for all the extra ink. 
-}else{
-this.excessInk = []; //reset the excessInk to zero if there's nothing left. 
-}
-
-}
-
-offloadInk(){
-if (this.excessInk.length>1){
-for (let excessInkMolecule of this.excessInk){
-let prob = random(0,1); 
-
-if (prob<0.5){
-excessInkMolecule.move(this.rightNeighbour.x); 
-}else{
-excessInkMolecule.move(this.leftNeighbour.x); 
-}
-}
-}
-
-}
-}
-
-//ink particle object. 
-class InkMolecule{
-constructor(x, y, d = cellWidth, h = cellHeight){
-this.x = x; 
-this.y = y; 
-this.d = d; 
-this.h = h; 
-}
-
-display(){
-noStroke(); 
-
-fill (0, 1); 
-rect (this.x, this.y, this.d, this.h); 
-}
-
-move(destX){
-this.x = lerp(this.x, destX, 0.1); 
-
-//stop lerp for tiny movements. 
-if (abs(this.x-this.destinationX)<0.1){
-this.x = destX; 
-}
-
-}
-
+function mouseClicked(){
+    for (let i = 0; i < numOfInkMolecules; i++) {
+        inkMolecules.push(new InkMolecule(mouseX, mouseY));
+    }
 }
