05e233da13bdcbb5f99897e6c8e4ee16b5e9d142
Arjun <arjunyadavdesigns@gmail.com>
2025-02-20 22:35:56 +0530

    arjun tried and failed and stopped

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
-//inkBlot_1d; february, 2025. 
+//inkblot, but only with math; february 2025. 
 
-//shobhan explained his logic for the ink blot and asked me to create a one-dimensional simulation. 
+/*
+//logic: 
+
+*/
+
+/*p5.pixels reference
+- one dimensional array from 0,0 going horizontally (and then wrapping below). 
+- stores rgba values where for (0,0), pixels[0] = r, pixels[1] = g ...
+
+*/
 
 function setup() {
 createCanvas(1000, 562); //in 16:9 aspect ratio. 
+
+pixelDensity(1); 
+loadPixels(); 
 }
 
 function draw() {
 background(255);
+}
+
+function convertPixelsToTwoDimensionalArray(){
+for (var y = 0; y<height; y++){
+for (var x = 0; x<width; x++){
+var index = (x+y*width); 
+}
+}
 }
